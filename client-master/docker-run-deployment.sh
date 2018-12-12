476a018df0103b3df45cec5080d9006df28bafcb
Vic Ding <qiqing.ding@meertens.knaw.nl>
2018-12-12 11:00:10 +0100

    auth working, having bad group member error with required group clarin error

--- client-master/docker-run-deployment.sh
+++ client-master/docker-run-deployment.sh
@@ -8,5 +8,8 @@ cp target/PlateClient.war /usr/local/tomcat/webapps/PlateClient.war
 
 /usr/local/tomcat/bin/catalina.sh run
 
+rm -fr /usr/local/tomcat/webapps/ROOT
+cp target/PlateClient.war /usr/local/tomcat/webapps/ROOT.war
+
 # keep docker running when redeploying:
 tail -f /dev/null
